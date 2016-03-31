622a6fdd3a03f8b02274b243eaaf9ba80bf02b01
Guiyan Bai <gyb2015@uw.edu>
2016-03-31 13:22:30 -0700

    submit files

---
+++ helloios/ViewSecond.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  helloios
+//
+//  Created by User on 3/31/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewSecond: UIViewController {
+    @IBOutlet var Label: UILabel!
+
+}
